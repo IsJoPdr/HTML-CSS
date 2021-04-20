73c6ea490ec0803e13abfe4d2bfa60adde1bdb45
pdr <82740834+pdrgg@users.noreply.github.com>
2021-04-20 10:30:16 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const Model = {
+      open() {
+        document.querySelector(".model-ov")
+        .classList.add("act")
+      },
+      close() {
+       document.querySelector(".model-ov")
+        .classList.remove("act")
+      }
+    }
